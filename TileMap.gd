747ea705ececfcb9cb62e12cb9eb0022fc31a46b
laketrout <averagelaketrout@proton.me>
2023-10-31 19:41:46 +0200

    Place and break blocks

--- TileMap.gd
+++ TileMap.gd
@@ -1,10 +1,13 @@
 extends TileMap
 
+const SELECTED_BLOCK = Vector2(3, 1) # dirt for now
+
+func selected_tile() -> Vector2:
+	return local_to_map(get_global_mouse_position())
+
 func _process(delta: float) -> void:
-	if (Input.is_action_just_pressed("left_click")):
-		var tile: Vector2 = local_to_map(get_global_mouse_position())
-		set_cell(0, tile, 0, Vector2i.ZERO)
+	if (Input.is_action_pressed("left_click")):
+		erase_cell(0, selected_tile())
 	
-	if (Input.is_action_just_pressed("right_click")):
-		var tile: Vector2 = local_to_map(get_global_mouse_position())
-		erase_cell(0, tile)
+	if (Input.is_action_pressed("right_click")):
+		set_cell(0, selected_tile(), 0, SELECTED_BLOCK)

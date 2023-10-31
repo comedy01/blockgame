b40f32020adbe6c55d0df62418f7a50a617733dd
laketrout <averagelaketrout@proton.me>
2023-10-31 18:40:46 +0200

    Simplify horizontal movement and add type annotations

--- TileMap.gd
+++ TileMap.gd
@@ -1,11 +1,10 @@
 extends TileMap
 
-func _process(delta):
+func _process(delta: float) -> void:
 	if (Input.is_action_just_pressed("left_click")):
-		var tile : Vector2 = local_to_map(get_global_mouse_position())
-		set_cell(0 , tile, 0, Vector2i.ZERO)
+		var tile: Vector2 = local_to_map(get_global_mouse_position())
+		set_cell(0, tile, 0, Vector2i.ZERO)
 	
 	if (Input.is_action_just_pressed("right_click")):
-		var tile : Vector2 = local_to_map(get_global_mouse_position())
+		var tile: Vector2 = local_to_map(get_global_mouse_position())
 		erase_cell(0, tile)
-	
--- player.gd
+++ player.gd
@@ -1,25 +1,20 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-const MAX_JUMPS = 2
+const SPEED: float = 300.0
+const JUMP_VELOCITY: float = -400.0
+const MAX_JUMPS: int = 2
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var jumps = 0
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+var jumps: int = 0
 
-func _draw():
+func _draw() -> void:
 	draw_rect(Rect2(Vector2(0, 0), Vector2(26, 26)), Color(1, 0, 0), false)
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
-			
-	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity.x = SPEED * (Input.get_action_strength("move_right") - Input.get_action_strength("move_left"))
 
 	if is_on_floor():
 		jumps = 0
@@ -27,5 +22,5 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("move_up") and (is_on_floor() or jumps < MAX_JUMPS):
 		velocity.y = JUMP_VELOCITY
 		jumps += 1
-		
+
 	move_and_slide()

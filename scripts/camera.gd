16e4857d2dc9d5003e4adb505ca6d6f279f78c50
laketrout <averagelaketrout@proton.me>
2023-11-01 20:06:43 +0200

    Use delta value for zoom speed

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,15 +1,9 @@
 extends Camera2D
 
-var zoom_speed = 0.02
+const ZOOM_SPEED: float = 1.01
 
-func _process(delta):
+func _process(delta: float) -> void:
 	if Input.is_action_pressed("zoom_in"):
-		zoom_in()
+		zoom *= (ZOOM_SPEED + delta)
 	if Input.is_action_pressed("zoom_out"):
-		zoom_out()
-
-func zoom_in():
-		zoom *= (1.0 + zoom_speed)
-
-func zoom_out():
-		zoom /= (1.0 + zoom_speed)
+		zoom /= (ZOOM_SPEED + delta)

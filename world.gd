06a4af065bb701865eca50a2afa418e90af184e4
laketrout <averagelaketrout@proton.me>
2024-06-05 14:18:08 +0300

    improve

--- world.gd
+++ world.gd
@@ -2,30 +2,26 @@ extends Node2D
 
 @onready var tile_map = $TileMap
 
-var BLOCK_LAYER : int = 0
-var SOURCE_ID : int = 0
-var EMPTY_CELL = -1
+const SOURCE_ID = 0
+const BLOCK_LAYER = 0
+const GRASS_BLOCK_ATLAS = Vector2i(0, 0)
 
 func _ready():
 	pass
 	
-func _process(delta):
+func _process(_delta):
 	pass
 
-func is_tile_empty(mouse_cell_position : Vector2i) -> bool:
-	var cell_id = tile_map.get_cell_source_id(BLOCK_LAYER, mouse_cell_position, false)
-	return cell_id == -1
+func is_tile_empty(pos: Vector2i) -> bool:
+	return tile_map.get_cell_source_id(BLOCK_LAYER, pos, false) == -1
 
+func get_mouse_tile() -> Vector2i:
+	return tile_map.local_to_map(get_global_mouse_position())
 
 func _input(event):
-	if Input.is_action_just_pressed("leftclick"):
-		var mouse_position : Vector2 = get_global_mouse_position()
-		var mouse_cell_position : Vector2i = tile_map.local_to_map(mouse_position)
-		if is_tile_empty(mouse_cell_position):
-			var atlas_coords : Vector2i = Vector2i(0,0) #grass block atlas hcoords for now, can add block changing later
-			tile_map.set_cell(BLOCK_LAYER, mouse_cell_position, SOURCE_ID, atlas_coords)
-	
-	elif Input.is_action_just_pressed("rightclick"):
-		var mouse_position : Vector2 = get_global_mouse_position()
-		var mouse_cell_position : Vector2i = tile_map.local_to_map(mouse_position)
-		tile_map.erase_cell(BLOCK_LAYER, mouse_cell_position)
+	if event.is_action_pressed("left_click"):
+		var mouse_tile = get_mouse_tile()
+		if is_tile_empty(mouse_tile):
+			tile_map.set_cell(BLOCK_LAYER, mouse_tile, SOURCE_ID, GRASS_BLOCK_ATLAS)
+	elif event.is_action_pressed("right_click"):
+		tile_map.erase_cell(BLOCK_LAYER, get_mouse_tile())

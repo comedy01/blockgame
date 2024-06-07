5a1bf4b0f40775bba8bf68377205795597fea673
laketrout <averagelaketrout@proton.me>
2024-06-07 17:54:46 +0300

    improve terrain gen

--- scripts/world.gd
+++ scripts/world.gd
@@ -3,61 +3,62 @@ extends Node2D
 @onready var tile_map = $TileMap
 @export var noise_map : Texture2D
 
-const SOURCE_ID: int = 0
-const BLOCK_LAYER: int = 0
+const SOURCE_ID = 0
+const BLOCK_LAYER = 0
 const GRASS_BLOCK_ATLAS = Vector2i(0, 0)
 const DIRT_BLOCK_ATLAS = Vector2i(1,0)
 const STONE_BLOCK_ATLAS = Vector2i(2,0)
 const GRASS_RAMP_RIGHT_ATLAS = Vector2i(10,0)
 const GRASS_RAMP_LEFT_ATLAS = Vector2i(11,0)
+const WORLD_SIZE = Vector2i(2000, 500)
 
-const WORLD_WIDTH: int = 2000
-const WORLD_HEIGHT: int = 500
+func get_noise_i(noise: FastNoiseLite, x: int) -> int:
+	return int(noise.get_noise_1d(x) * 10)
 
-func calculate_stone_offset(current_stone_offset: int, stone_y: int, dirt_height: int) -> int:
-	if (dirt_height - 2 > stone_y + current_stone_offset):
-		return randi_range(0,3)
-	var offset_chance = randi_range(0,100)
-	if (offset_chance <= 80):
-		return randi_range(-1,1)
-	elif (offset_chance > 80 and offset_chance <= 86):
-		return -2
-	elif (offset_chance > 86 and offset_chance <= 92):
-		return 2
-	elif (offset_chance > 92 and offset_chance <= 96):
-		return -3
-	else:
-		return 3	
+func set_block(x: int, y: int, atlas: Vector2i) -> void:
+	tile_map.set_cell(BLOCK_LAYER, Vector2i(x, y), SOURCE_ID, atlas)
+
+func make_layer_offset() -> int:
+	match randi_range(1, 20):
+		1:    return randi_range(-3, 3)
+		2, 3: return randi_range(-2, 2)
+		_:    return randi_range(-1, 1)
+
+func set_grass(x: int, y: int, previous_y: int) -> void:
+	match previous_y - y:
+		1:
+			set_block(x, y, GRASS_RAMP_RIGHT_ATLAS)
+		-1:
+			set_block(x, y - 1, GRASS_RAMP_LEFT_ATLAS)
+			set_block(x, y, DIRT_BLOCK_ATLAS)
+		_:
+			set_block(x, y, GRASS_BLOCK_ATLAS)
 
 func _ready():
-	var noise : FastNoiseLite = noise_map.noise
-	var stone_y: int = randi_range(1,20)
-	var current_stone_offset = 0
-	var last_dirt_height = null
-	var dirt_height = null
-	for x in WORLD_WIDTH:
-		last_dirt_height = dirt_height
-		dirt_height = int(noise.get_noise_1d(x) * 10 )
-			
-		current_stone_offset = calculate_stone_offset(current_stone_offset, stone_y, dirt_height)
-		tile_map.set_cell(BLOCK_LAYER, Vector2(x, dirt_height), 0, GRASS_BLOCK_ATLAS)
-		tile_map.set_cell(BLOCK_LAYER, Vector2(x, stone_y + current_stone_offset),0, STONE_BLOCK_ATLAS)
-		stone_y += current_stone_offset
-		
-		if (dirt_height + 1 == last_dirt_height and tile_map.get_cell_atlas_coords(BLOCK_LAYER, Vector2(x, dirt_height)) == GRASS_BLOCK_ATLAS) :
-			tile_map.set_cell(BLOCK_LAYER, Vector2(x, dirt_height), 0, GRASS_RAMP_RIGHT_ATLAS)
-		elif (dirt_height - 1 == last_dirt_height and tile_map.get_cell_atlas_coords(BLOCK_LAYER, Vector2(x-1, dirt_height-1),) == GRASS_BLOCK_ATLAS):
-			tile_map.set_cell(BLOCK_LAYER, Vector2(x-1, dirt_height -1 ), 0, GRASS_RAMP_LEFT_ATLAS)
-		
-		for y in range(dirt_height + 1, stone_y): #fill in gap between stone and grass layer with dirt
-			tile_map.set_cell(BLOCK_LAYER, Vector2(x, y), 0, DIRT_BLOCK_ATLAS)
-		for y in 30: #chose random stone depth we can adjust later
-			tile_map.set_cell(BLOCK_LAYER, Vector2(x,stone_y + y),0, STONE_BLOCK_ATLAS)
-	
-		
-func _process(_delta):
-	pass 
-	
+	var noise: FastNoiseLite = noise_map.noise
+	var stone_y: int = randi_range(1, 20)
+
+	for x in WORLD_SIZE.x:
+		var grass_y = get_noise_i(noise, x)
+
+		stone_y = clamp(stone_y + make_layer_offset(), grass_y - 5, grass_y + 10)
+		if stone_y < grass_y: stone_y += randi_range(0, 1)
+
+		# Grass surface
+		set_grass(x, grass_y, get_noise_i(noise, x - 1))
+
+		# Dirt layer
+		for y in range(grass_y + 1, stone_y):
+			tile_map.set_cell(BLOCK_LAYER, Vector2i(x, y), 0, DIRT_BLOCK_ATLAS)
+
+		# Stone layer
+		for y in 30:
+			tile_map.set_cell(BLOCK_LAYER, Vector2i(x, stone_y + y), 0, STONE_BLOCK_ATLAS)
+
+
+func _process(_delta) -> void:
+	pass
+
 func is_tile_empty(pos: Vector2i) -> bool:
 	return tile_map.get_cell_source_id(BLOCK_LAYER, pos, false) == -1
 
@@ -66,8 +67,9 @@ func get_mouse_tile() -> Vector2i:
 
 func _input(event):
 	if event.is_action_pressed("right_click"):
-		var mouse_tile = get_mouse_tile()
-		if is_tile_empty(mouse_tile):
-			tile_map.set_cell(BLOCK_LAYER, mouse_tile, SOURCE_ID, GRASS_BLOCK_ATLAS)
+		var tile = get_mouse_tile()
+		if is_tile_empty(tile):
+			tile_map.set_cell(BLOCK_LAYER, tile, SOURCE_ID, GRASS_BLOCK_ATLAS)
+
 	elif event.is_action_pressed("left_click"):
 		tile_map.erase_cell(BLOCK_LAYER, get_mouse_tile())

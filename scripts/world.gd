f904284d947809a0984c93b3d0bfaa23938eb592
comedy01 <145397270+comedy01@users.noreply.github.com>
2024-06-11 00:31:41 +0200

    completely reworked generation

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 @onready var tile_map = $TileMap
-@export var noise_map : Texture2D
+@export var noise_map_grass : Texture2D
+@export var noise_map_dirt : Texture2D
+@export var noise_map_caves: Texture2D
 
 const SOURCE_ID = 0
 const BLOCK_LAYER = 0
@@ -12,8 +14,14 @@ const GRASS_RAMP_RIGHT_ATLAS = Vector2i(10,0)
 const GRASS_RAMP_LEFT_ATLAS = Vector2i(11,0)
 const WORLD_SIZE = Vector2i(2000, 500)
 
-func get_noise(x: int) -> int:
-	return int(noise_map.noise.get_noise_1d(x) * 10)
+func get_noise_grass(x: int) -> int:
+	return int(noise_map_grass.noise.get_noise_1d(x) * 10)
+	
+func get_noise_dirt(x, y) -> int:
+	return int(noise_map_dirt.noise.get_noise_2d(x,y) * 10)
+
+func get_noise_caves(x, y) -> int:
+	return noise_map_caves.noise.get_noise_2d(x,y) * 10	
 
 func set_block(x: int, y: int, atlas: Vector2i) -> void:
 	tile_map.set_cell(BLOCK_LAYER, Vector2i(x, y), SOURCE_ID, atlas)
@@ -33,26 +41,47 @@ func set_grass(x: int, y: int, previous_y: int) -> void:
 			set_block(x, y, DIRT_BLOCK_ATLAS)
 		_:
 			set_block(x, y, GRASS_BLOCK_ATLAS)
+			
+func generate_underground(stone_index: int, x: int, y: int, grass_y: int):
+	var dirt_noise: int = get_noise_dirt(x,y)
+	if (dirt_noise > stone_index):
+		set_block(x,y+grass_y,DIRT_BLOCK_ATLAS)
+	else:
+		set_block(x,y+grass_y, STONE_BLOCK_ATLAS)
+	if (y > 25 and y < 30):
+		return -3
+	elif (y > 30 and y < 40):
+		return -2
+	elif (y > 40 and y < 50):
+		return -1
+	if (y > 50 and y < 60):
+		return 0
+	elif (y > 60 and y < 70):
+		return 1
+	elif (y > 70 and y < 80):
+		return 2
+	elif (y > 80 and y < 90):
+		return 3
+	else: 
+		return stone_index
 
 func _ready():
-	var stone_y: int = randi_range(1, 20)
+	var cave_noise: FastNoiseLite = noise_map_caves.noise
+	var stone_index: int = -4
 
 	for x in WORLD_SIZE.x:
-		var grass_y = get_noise(x)
-
-		stone_y = clamp(stone_y + make_layer_offset(), grass_y - 5, grass_y + 10)
-		if stone_y < grass_y: stone_y += randi_range(0, 1)
-
+		stone_index = -4
+		var grass_y = get_noise_grass(x)
+		for y in 100-grass_y:
+			var new_stone_index = generate_underground(stone_index, x, y, grass_y)
+			stone_index = new_stone_index
+			
 		# Grass surface
-		set_grass(x, grass_y, get_noise(x - 1))
-
-		# Dirt layer
-		for y in range(grass_y + 1, stone_y):
-			tile_map.set_cell(BLOCK_LAYER, Vector2i(x, y), 0, DIRT_BLOCK_ATLAS)
-
-		# Stone layer
-		for y in 30:
-			tile_map.set_cell(BLOCK_LAYER, Vector2i(x, stone_y + y), 0, STONE_BLOCK_ATLAS)
+		set_grass(x, grass_y, get_noise_grass(x - 1))
+		
+		for y in WORLD_SIZE.y:
+			if (cave_noise.get_noise_2d(x,y) < -0.7):
+				tile_map.erase_cell(BLOCK_LAYER, Vector2i(x, y+grass_y-1))
 
 func _process(_delta) -> void:
 	pass
